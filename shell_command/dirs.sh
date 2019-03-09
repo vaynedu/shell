26a18c5d1844be3d160d915c4454f813f4ee281a
lotluck <1219345363@qq.com>
2019-03-09 23:56:11 +0800

    add dirs pushd popd command

---
+++ shell_command/dirs.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+
+dirs -v
+#0  /data1/shell/shell_command
+
+#push dir to stack and switch current dir
+echo "--------------------pushd-----------------------"
+pushd /data1/shell
+pwd
+pushd /data1/shell/shell_script
+pwd
+pushd /data1/shell/shell_script
+pwd
+dirs -v
+
+
+pushd +2
+pushd -1
+
+dirs -v
+
+#/data1/shell /data1/shell/shell_command
+#/data1/shell
+#/data1/shell/shell_script /data1/shell /data1/shell/shell_command
+#/data1/shell/shell_script
+#/data1/shell/shell_script /data1/shell/shell_script /data1/shell /data1/shell/shell_command
+#/data1/shell/shell_script
+# 0  /data1/shell/shell_script
+# 1  /data1/shell/shell_script
+# 2  /data1/shell
+# 3  /data1/shell/shell_command
+#/data1/shell /data1/shell/shell_command /data1/shell/shell_script /data1/shell/shell_script
+#/data1/shell/shell_script /data1/shell/shell_script /data1/shell /data1/shell/shell_command
+# 0  /data1/shell/shell_script
+# 1  /data1/shell/shell_script
+# 2  /data1/shell
+# 3  /data1/shell/shell_command
+
+
+#pop dir to stach
+echo "----------------------popd-------------------------"
+
+popd 
+
+popd +1
+
+dirs -v
+#/data1/shell/shell_script /data1/shell /data1/shell/shell_command
+#/data1/shell/shell_script /data1/shell/shell_command
+# 0  /data1/shell/shell_script
+# 1  /data1/shell/shell_command
+
