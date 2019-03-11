dc1c84f8494eb7bf74a7aaacc00110896e116126
lotluck <1219345363@qq.com>
2019-03-11 17:38:30 +0800

    find and grep

---
+++ shell_command/find.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+#only show dirs
+
+find -type d 
+
+ls -l | grep "^d" | awk '{print $9}'
+
+ls -F | grep "/$"
+
+#only show files
+
+find -type f
+
+ls -l | grep "^-" | awk '{print $9}'
+
+
+
+# search empty dir and delelte
+
+find -type d -empty | xargs rm -fr {};
+
+
+#delete designated size files
+find . -name "*" -type f size 0c | xargs -n 1 rm -f
+
+
+# delete file  0.2 day ago
+find . -mtime +0.2 -name "*.log.*" -exec rm {} \;
+
+
+
+# search file, and list. in fact, grep is better
+find . -type f -name "bu_conf_*.conf" | xargs ls
+grep -rn "bu_conf_*.conf" bu/
+
+
+
