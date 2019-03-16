3e62ab82a433f2cf8e1f21b265eb05f25224a3cb
lotluck <1219345363@qq.com>
2019-03-17 00:51:10 +0800

    判断奇数和偶数

---
+++ shell_script/judge_odd_or_even.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#input a number, judging the num is  odd or even
+
+read -p "please a number : " n
+#n1=$((n%2))
+
+
+n2=`echo $n | grep -c [^0-9]`
+
+if [ $n2 -eq 1 ]
+then
+    echo "there is no number"
+    exit
+fi
+
+n1=$[$n%2]
+if [ $n1 -eq 0 ]
+then
+    echo "odd"
+else
+    echo "even"
+fi
