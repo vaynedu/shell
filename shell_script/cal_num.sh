d7722526b6743320a2aa253509a78bcc85029275
lotluck <1219345363@qq.com>
2019-03-17 00:50:09 +0800

    shell常用计算命令

---
+++ shell_script/cal_num.sh
@@ -0,0 +1,18 @@
+# /bin/bash 
+
+#shell calculate
+
+int_a=50
+int_b=10
+
+let num=$(($int_a-$int_b))
+let rdidx=$(($RANDOM % $num))+262144
+
+echo  "$num, $rdidx"
+
+vaynedu_num=10
+num=`python -c "print $vaynedu_num*0.2"`
+stime=`python -c "print '%d'%($vaynedu_num*0.2)"`
+echo $num
+echo $stime
+
