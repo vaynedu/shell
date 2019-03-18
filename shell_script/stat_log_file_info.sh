f4d4d53b55aca2848d20aee6c8144f47fae0c255
lotluck <1219345363@qq.com>
2019-03-18 16:23:13 +0800

    统计文件信息

---
+++ shell_script/stat_log_file_info.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+
+# 2019_03_16_23_00.txt
+# 2019_03_16_23_05.txt
+# 2019_03_16_23_10.txt
+# 2019_03_16_23_15.txt
+# 2019_03_16_23_20.txt
+# 2019_03_16_23_25.txt
+# 2019_03_16_23_30.txt
+# 2019_03_16_23_35.txt
+# 2019_03_16_23_40.txt
+# 2019_03_16_23_45.txt
+# 2019_03_16_23_50.txt
+# 2019_03_16_23_55.txt
+
+
+y=03
+m=17
+
+
+
+file_name=global_file_info.txt
+
+
+ls -l  | grep 2019_${y}_${m}_ |  awk 'BEGIN{size=0;c=0}{size+=$5;c+=1}END{printf "file  num : %d \nfile size : %.2fG\n", c,size/1024/1024/1024}' >> $file_name
+
+echo >> $file_name
+
+find . -type f -name "2019_${y}_${m}_[0-9][0-9]_[0-9][0-9].txt" | xargs wc -l  >> $file_name
+
+echo >> $file_name
