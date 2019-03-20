c3940e9aeaee6f9263eed0bf1ef443715e2979f8
lotluck <1219345363@qq.com>
2019-03-20 19:35:21 +0800

    awk usage

---
+++ shell_command/awk.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#match last char
+echo "5.3G" | awk '{n=split($0,a,"");print a[n]}'
+echo "5.3M" | awk '{print substr($0,length($0))}'
+
+
+#cal tatal file size
+ls -l | awk 'BEGIN{size=0;c=0}{size+=$5;c+=1}END{printf "file  num : %d \nfile size : %.2fG\n", c,size/1024/1024/1024}'
+
+#
--- shell_script/stat_log_file_info.sh
+++ shell_script/stat_log_file_info.sh
@@ -1,6 +1,25 @@
 #! /bin/bash
 
+y=03
+m=17
 
+
+
+file_name=global_file_info.txt
+
+#du -c 1.*
+ls -l  | grep 2019_${y}_${m}_ |  awk 'BEGIN{size=0;c=0}{size+=$5;c+=1}END{printf "file  num : %d \nfile size : %.2fG\n", c,size/1024/1024/1024}' >> $file_name
+
+echo >> $file_name
+
+find . -type f -name "2019_${y}_${m}_[0-9][0-9]_[0-9][0-9].txt" | xargs wc -l  >> $file_name
+
+echo >> $file_name
+
+
+
+
+#file as
 # 2019_03_16_23_00.txt
 # 2019_03_16_23_05.txt
 # 2019_03_16_23_10.txt
@@ -15,18 +34,4 @@
 # 2019_03_16_23_55.txt
 
 
-y=03
-m=17
 
-
-
-file_name=global_file_info.txt
-
-
-ls -l  | grep 2019_${y}_${m}_ |  awk 'BEGIN{size=0;c=0}{size+=$5;c+=1}END{printf "file  num : %d \nfile size : %.2fG\n", c,size/1024/1024/1024}' >> $file_name
-
-echo >> $file_name
-
-find . -type f -name "2019_${y}_${m}_[0-9][0-9]_[0-9][0-9].txt" | xargs wc -l  >> $file_name
-
-echo >> $file_name
