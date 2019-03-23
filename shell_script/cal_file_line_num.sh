2b9a5ca97edeb51d29c0255547085179e9316e88
lotluck <1219345363@qq.com>
2019-03-23 19:00:47 +0800

    计算文件的行数

---
+++ shell_script/cal_file_line_num.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+THREADNUM=16
+
+TMPFIFO=/tmp/$$.fifo
+[ -e $TMPFIFO ] || mkfifo $TMPFIFO
+exec 16<>${TMPFIFO}
+rm -fr ${TMPFIFO}
+
+for((i=1;i<=$THREADNUM;i++))
+do
+    echo
+done >&16
+
+
+month=03
+day=22
+
+
+
+file_name_list=`ls -l | awk '{print $9}' | grep "2019_${month}_${day}_"  | grep -E "^2019_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}.txt"`
+
+for i in $file_name_list
+do
+    read -u16
+    {
+      wc -l $i >> tmp.cal.txt
+      echo >&16
+    }&
+done
+
+wait
+exec 16>&-
+
