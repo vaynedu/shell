948b3d519342098033ad7462b93f72ffddcd103d
lotluck <1219345363@qq.com>
2019-03-30 22:01:13 +0800

    增加注释

---
+++ shell_easy_tools/README.md
@@ -0,0 +1,18 @@
+
+
+常用shell 命令的小工具
+====
+auto_update and last_modify: 2019-03-30 22:00:33
+-------
+## [1.auto_generate_dir_lish.sh](https://github.com/lotluck/shell/blob/master/shell_command/auto_generate_dir_lish.sh)
+>> 自动生成该目录下的文件信息
+## [2.shell_termimal_skill.txt](https://github.com/lotluck/shell/blob/master/shell_command/shell_termimal_skill.txt)
+>>  shell终端常用的小技巧命令
+## [3.tmux.txt](https://github.com/lotluck/shell/blob/master/shell_command/tmux.txt)
+>> tmux 终端复用神器, 常用操作命令
+## [4.vim.txt](https://github.com/lotluck/shell/blob/master/shell_command/vim.txt)
+>>  vim常用骚操作
+
+
+
+
---
+++ shell_easy_tools/auto_generate_dir_lish.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#自动生成该目录下的文件信息
+>./README.md
+echo -e "\n\n常用shell 命令的小工具" >> ./README.md
+echo "====" >> ./README.md
+echo -e "auto_update and last_modify: `date "+%Y-%m-%d %H:%M:%S"`" >> ./README.md
+echo "-------" >> ./README.md
+
+
+cnt=1
+
+file_list=`ls -l | awk '{print $9}'`
+for file in $file_list
+do
+      [ "$file"x == "README.md"x ]      && continue
+      [ "$file"x == "ChangeLog.txt"x ]  && continue
+               
+      echo "## [$cnt.$file](https://github.com/lotluck/shell/blob/master/shell_command/$file)" >> ./README.md         
+      if [ -d "$file" ]
+      then
+         echo ">> 目录文件请在里面查看" >> ./README.md
+      else
+         echo ">> `sed -n '3p' $file | awk -F '#' '{print $2}'`" >> ./README.md
+      fi
+      let cnt++
+done
+
+echo -e "\n\n\n"  >> ./README.md
